05dd81896b854259cf7917e9feccd28b3d65bfe0
Rémi Alvergnat <toilal.dev@gmail.com>
2018-01-03 08:33:10 +0100

    refactor(theme): Refactor theme properties to a TypeScript module

--- src/theme/font-awesome/Handle.vue
+++ src/theme/font-awesome/Handle.vue
@@ -16,21 +16,11 @@
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import props from '../themeProps'
 
-export default Vue.extend({
-  props: {
-    vm: Object, // The VueTreeNode component instance.
-    node: Object, // The data of the node to display.
-    label: String, // Label to display.
-    hidden: Boolean, // Should the node be hidden ?
-    leaf: Boolean, // Should the node be displayed as a leaf ?
-    opened: Boolean, // Should the node be opened ?
-    loading: Boolean, // Is the node currently loading ?
-    error: Object // Contains the error object if an error occurs while loading the node.
-  }
-})
+export default Vue.extend({ props })
 </script>
-<style class="scss" scoped>
+<style lang="scss" scoped>
 .vue-tree__tree-node-handle {
   font-weight: bold;
 }
---
+++ src/theme/themeProps.ts
@@ -0,0 +1,10 @@
+export default {
+  vm: Object, // The VueTreeNode component instance.
+  node: Object, // The data of the node to display.
+  label: String, // Label to display.
+  hidden: Boolean, // Should the node be hidden ?
+  leaf: Boolean, // Should the node be displayed as a leaf ?
+  opened: Boolean, // Should the node be opened ?
+  loading: Boolean, // Is the node currently loading ?
+  error: Object // Contains the error object if an error occurs while loading the node.
+}
