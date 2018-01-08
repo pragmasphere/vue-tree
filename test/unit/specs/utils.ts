0fcb4d70e885a7787a1c966e0adafffcdf2016b1
Rémi Alvergnat <toilal.dev@gmail.com>
2018-01-08 11:37:50 +0100

    refactor(test): Use vue-test-utils filter fork

--- test/unit/specs/utils.ts
+++ test/unit/specs/utils.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueTreeNode from '@/VueTreeNode.vue'
 
-import { createWrapperArray, Wrapper, WrapperArray } from 'vue-test-utils'
+import { Wrapper, WrapperArray } from 'vue-test-utils'
 
 export function isVisible (wrapper: Wrapper<any>) {
   const computedStyle = window.getComputedStyle(wrapper.element)
@@ -9,7 +9,7 @@ export function isVisible (wrapper: Wrapper<any>) {
 }
 
 export function visibleTreeNodes (wrapper: Wrapper<any>): WrapperArray<Vue> {
-  return createWrapperArray(wrapper.findAll(VueTreeNode).wrappers.filter(w => isVisible(w)))
+  return wrapper.findAll(VueTreeNode).filter(w => isVisible(w))
 }
 
 export function visibleText (wrapper: Wrapper<any>) {
